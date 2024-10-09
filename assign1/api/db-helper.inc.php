131e277929d8b4c264bb5c19e40ce7a46d50b238
daraakitoyemru <dakit711@mtroyal.ca>
2024-10-08 18:02:37 -0600

    testing db connection

--- assign1/api/db-helper.inc.php
+++ assign1/api/db-helper.inc.php
@@ -1,6 +1,15 @@
 <?php
 require_once './config.inc.php';
+try {
+    $pdo = new PDO(DBCONNSTRING);
 
+
+    $result = $pdo->query('Select * from circuits');
+    echo json_encode($result->fetchAll(PDO::FETCH_ASSOC), JSON_NUMERIC_CHECK);
+
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
 
 
 ?>
