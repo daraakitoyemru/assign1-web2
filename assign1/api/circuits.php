2573be545593318966fe52cea4944b96897c2487
daraakitoyemru <dakit711@mtroyal.ca>
2024-10-11 15:27:52 -0600

    added more endpoints, tested endpoints

--- assign1/api/circuits.php
+++ assign1/api/circuits.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './circuits.php';
+require_once './db-classes-inc.php';
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -9,14 +9,27 @@ header('Content-type: application/json');
 // indicate whether other domains can use this API
 header("Access-Control-Allow-Origin: *");
 
+
+
 try {
     $conn = DBHelper::createConnection(DBCONNSTRING);
 
     $circuitGateway = new Circuits($conn);
-    $results = $circuitGateway->getAllCircuits();
+    if (isCorrectQuery('ref')) {
+        $results = $circuitGateway->getCircuitsByName($_GET['ref']);
+    } else {
+        $results = $circuitGateway->getAllCircuits();
+    }
     echo json_encode($results, JSON_NUMERIC_CHECK, JSON_PRETTY_PRINT);
 } catch (PDOException $e) {
     die($e->getMessage());
+}
+function isCorrectQuery($param)
+{
+    if (isset($_GET[$param]) && !empty($_GET[$param])) {
+        return true;
+    }
+    return false;
 }
 
 ?>
--- assign1/api/db-classes-inc.php
+++ assign1/api/db-classes-inc.php
@@ -4,7 +4,7 @@ require_once './db-helper.inc.php';
 
 class Circuits
 {
-
+    private static $baseSQl = "SELECT circuitId,circuitRef,name,location,country,lat,lng,alt,url FROM circuits";
     private $pdo;
     public function __construct($conn)
     {
@@ -13,8 +13,17 @@ class Circuits
     public function getAllCircuits()
     {
 
-        $sql = "SELECT circuitId,circuitRef,name,location,country,lat,lng,alt,url FROM circuits";
+        $sql = self::$baseSQl;
+        //$sql2 = "SELECT circuitId,circuitRef,name,location,country,lat,lng,alt,url FROM circuits";
+
         $statement = DBHelper::runQuery($this->pdo, $sql, null);
+        return $statement->fetchAll();
+    }
+
+    public function getCircuitsByName($circuitName)
+    {
+        $sql = self::$baseSQl . " WHERE circuitRef=?";
+        $statement = DBHelper::runQuery($this->pdo, $sql, $circuitName);
         return $statement->fetchAll();
     }
 }
