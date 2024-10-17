09ed3ce4a886c48b248fa364c74fb8fde877bb2a
vbolg790 <vbolg790@mtroyal.ca>
2024-10-17 12:53:14 -0600

    index.php inserted and partially stylized

---
+++ assign1/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COMP3512 Assignment #1</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+<header>
+    <h1>F1 DASHBOARD PROJECT</h1>
+    <nav>
+        <a href="#home">Home</a>
+        <a href="#browse">Browse</a>
+        <a href="#apis">APIs</a>
+    </nav>
+</header>
+
+<main>
+    <aside>
+        <p>This website was created for Assignment 1 of COMP3512 at Mount Royal University. 
+            The focus of the project is to explore web development technologies such as HTML, CSS, and PHP
+            we can add more description stuff later</p>
+        <p class="group-members">Group Members: Dara, Viktoriya</p>
+        <a href="https://github.com/daraakitoyemru/assign1-web2" target="_blank" class="github-link" >GitHub Repository</a> <br/>
+        <a href="#browse" class="browse-btn">Browse</a>
+    </aside>
+
+    <section class="main-content">
+        <img src="images/race_car.jpg" alt="Race car" class="race-car-img">
+    </section>
+</main>
+
+<footer>
+    &copy; 2024 COMP3512 Assignment #1. All Rights Reserved.
+</footer>
+
+</body>
+</html>
