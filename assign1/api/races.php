5a5494463311ca4af33ed6af640592b7c272ab08
daraakitoyemru <dakit711@mtroyal.ca>
2024-10-17 13:49:16 -0600

    adds races api

---
+++ assign1/api/races.php
@@ -0,0 +1,19 @@
+<?php
+require_once './db-classes-inc.php';
+require '../helpers/helperFunc.inc.php';
+header('Content-type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+try {
+    $conn = DBHelper::createConnection(DBCONNSTRING);
+    $racesGateway = new Races($conn);
+
+    if (isCorrectQuery('ref')) {
+        $results = $racesGateway->getRacesbyRef($_GET['ref']);
+    } else {
+        $results = $racesGateway->getRaces2022();
+    }
+    echo json_encode($results, JSON_NUMERIC_CHECK, JSON_PRETTY_PRINT);
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
