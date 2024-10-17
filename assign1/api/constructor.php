22a9dcd3e32220495738c7f81808455d9e116762
daraakitoyemru <dakit711@mtroyal.ca>
2024-10-17 13:49:54 -0600

    minor fixes, new qualifying class

--- assign1/api/constructor.php
+++ assign1/api/constructor.php
@@ -1,9 +1,5 @@
 <?php
 require_once './db-classes-inc.php';
-require_once './db-helper.inc.php';
-require_once './config.inc.php';
-
-
 require '../helpers/helperFunc.inc.php';
 header('Content-type: application/json');
 header("Access-Control-Allow-Origin: *");
@@ -11,7 +7,7 @@ header("Access-Control-Allow-Origin: *");
 try {
     $conn = DBHelper::createConnection(DBCONNSTRING);
     $constructorGateway = new Constructor($conn);
-    if (isCorrectQuery($_GET['ref'])) {
+    if (isCorrectQuery('ref')) {
         $constructorResults = $constructorGateway->getConstructorByRef($_GET['ref']);
     } else {
         $constructorResults = $constructorGateway->getAllContstructors();
