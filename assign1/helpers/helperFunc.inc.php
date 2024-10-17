bf575c5c1fe59705c672edeca05d7fcb115a5dfc
daraakitoyemru <dakit711@mtroyal.ca>
2024-10-17 15:41:20 -0600

    adds function for easy object creation

--- assign1/helpers/helperFunc.inc.php
+++ assign1/helpers/helperFunc.inc.php
@@ -1,8 +1,26 @@
 <?php
+require_once './db-classes-inc.php';
 function isCorrectQuery($param)
 {
     if (isset($_GET[$param]) && !empty($_GET[$param])) {
         return true;
     }
     return false;
+}
+
+// creates an instance of a given object and returns it
+function getGateway($classname)
+{
+    try {
+        if (!class_exists($classname)) {
+            throw new Exception("Class $classname does not exist.");
+        }
+        $conn = DBHelper::createConnection(DBCONNSTRING);
+        $gateway = new $classname($conn);
+        return $gateway;
+    } catch (Exception $e) {
+        die($e->getMessage());
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
 }
