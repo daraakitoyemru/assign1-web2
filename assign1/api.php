4cfa2ec98a6d62e050610b737c72bd9c42709828
vbolg790 <vbolg790@mtroyal.ca>
2024-10-17 14:40:51 -0600

    all the needed apis are on the api page

--- assign1/api.php
+++ assign1/api.php
@@ -23,9 +23,17 @@
         <div class="column-1">
             <h2>URL</h2>
             <a href="">/api/circuits.php</a>
-            <a href="">/api/circuits.php?ref=monaco</a>
+            <a href="">/api/circuits.php?ref=</a>
             <a href="">/api/constructors.php</a>
-            <a href="">/api/constructors.php?ref=mclaren</a>
+            <a href="">/api/constructors.php?ref=</a>
+            <a href="">/api/drivers.php</a>
+            <a href="">/api/drivers.php?ref=?</a>
+            <a href="">/api/drivers.php?race=?</a>
+            <a href="">/api/races.php?ref=?</a>
+            <a href="">/api/races.php</a>
+            <a href="">/api/qualifying.php?ref=?</a>
+            <a href="">/api/results.php?ref=?</a>
+            <a href="">/api/results.php?driver=?</a>
         </div>
         <div class="column-2">
             <h2>Description</h2>
@@ -33,6 +41,14 @@
             <p>Return just a specific circuit</p>
             <p>Returns all the constructors</p>
             <p>Returns just a specific constructor</p>
+            <p>Returns all the drivers for the season</p>
+            <p>Returns just the specified driver</p>
+            <p>Returns the drivers within a given race</p>
+            <p>Returns just the specified race</p>
+            <p>Returns the races within the 2022 season ordered by round</p>
+            <p>Returns the qualifying results for the specified race</p>
+            <p>Returns the results for the specified race</p>
+            <p>Returns all the results for a given driver</p>
         </div>
     </section>
 </main>
