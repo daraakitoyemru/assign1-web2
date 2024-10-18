fc0efbe72f730eb0384a33924e079d465f5cbd7b
daraakitoyemru <dakit711@mtroyal.ca>
2024-10-18 16:08:59 -0600

    added some links

--- assign1/api.php
+++ assign1/api.php
@@ -24,18 +24,18 @@
         <section class="main-content">
             <div class="column-1">
                 <h2>URL</h2>
-                <a href="">/api/circuits.php</a>
-                <a href="">/api/circuits.php?circuitRef=</a>
-                <a href="">/api/constructors.php</a>
-                <a href="">/api/constructors.php?constructorRef=</a>
-                <a href="">/api/drivers.php</a>
-                <a href="">/api/drivers.php?driverRef=?</a>
-                <a href="">/api/drivers.php?race=?</a>
-                <a href="">/api/races.php?racesRef=?</a>
+                <a href="api/circuits.php">/api/circuits.php</a>
+                <a href="api/circuits.php?circuitRef=sepang<">/api/circuits.php?circuitRef=sepang</a>
+                <a href="/api/constructors.php">/api/constructors.php</a>
+                <a href="/api/constructors.php?constructorRef=mclaren">/api/constructors.php?constructorRef=mclaren</a>
+                <a href="/api/drivers.php">/api/drivers.php</a>
+                <a href="/api/drivers.php?driverRef=norris">/api/drivers.php?driverRef=norris</a>
+                <a href="/api/drivers.php?race=1106">/api/drivers.php?race=?</a>
+                <a href="/api/races.php?racesRef=">/api/races.php?racesRef=</a>
                 <a href="">/api/races.php</a>
                 <a href="">/api/qualifying.php?qualRef=?</a>
                 <a href="">/api/results.php?resRef=?</a>
-                <a href="">/api/results.php?driver=?</a>
+                <a href="">/api/results.php?driver=lando_norris</a>
             </div>
             <div class="column-2">
                 <h2>Description</h2>
