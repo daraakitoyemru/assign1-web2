516a33fd9ff6cdc9d75d8c6ead19d4a3c78b414e
vbolg790 <vbolg790@mtroyal.ca>
2024-10-18 11:05:24 -0600

    clicking a button now pulls up information about the race!

--- assign1/api/config.inc.php
+++ assign1/api/config.inc.php
@@ -1,7 +1,9 @@
 <?php
 
-
-define('DBCONNSTRING', 'sqlite:../data/f1.db');
-//define('DBCONNSTRING', 'sqlite:/Applications/XAMPP/xamppfiles/htdocs/dakit711/assign1-web2/assign1/data/f1.db');
+if (PHP_OS_FAMILY === 'Windows') {
+    define('DBCONNSTRING', 'sqlite:./data/f1.db');
+} else {
+    define('DBCONNSTRING', 'sqlite:' . realpath(__DIR__ . '/../data/f1.db'));
+}
 
 ?>
--- assign1/index.php
+++ assign1/index.php
@@ -1,40 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>COMP3512 Assignment #1</title>
     <link rel="stylesheet" href="index.css">
 </head>
+
 <body>
 
-<header>
-    <h1>F1 DASHBOARD PROJECT</h1>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="browse.php">Browse</a>
-        <a href="api.php">APIs</a>
-    </nav>
-</header>
+    <header>
+        <h1>F1 DASHBOARD PROJECT</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="browse.php">Browse</a>
+            <a href="api.php">APIs</a>
+        </nav>
+    </header>
 
-<main>
-    <aside>
-        <p>This website was created for Assignment 1 of COMP3512 at Mount Royal University. 
-            The focus of the project is to explore web development technologies such as HTML, CSS, and PHP
-            we can add more description stuff later</p>
-        <p class="group-members">Group Members: Dara, Viktoriya</p>
-        <a href="https://github.com/daraakitoyemru/assign1-web2" target="_blank" class="github-link"><strong>GitHub Repository</strong></a> <br/>
-        <a href="browse.php" class="browse-btn">Browse</a>
-    </aside>
+    <main>
+        <aside>
+            <p>This website was created for Assignment 1 of COMP3512 at Mount Royal University.
+                The focus of the project is to explore web development technologies such as HTML, CSS, and PHP
+                we can add more description stuff later</p>
+            <p class="group-members">Group Members: Dara, Viktoriya</p>
+            <a href="https://github.com/daraakitoyemru/assign1-web2" target="_blank" class="github-link"><strong>GitHub
+                    Repository</strong></a> <br />
+            <a href="browse.php" class="browse-btn">Browse</a>
+        </aside>
 
-    <section class="main-content">
-        <img src="images/race_car.jpg" alt="Race car" class="race-car-img">
-    </section>
-</main>
+        <section class="main-content">
+            <img src="images/race_car.jpg" alt="Race car" class="race-car-img">
+        </section>
+    </main>
 
-<footer>
-    &copy; 2024 COMP3512 Assignment #1.
-</footer>
+    <footer>
+        &copy; 2024 COMP3512 Assignment #1.
+    </footer>
 
 </body>
+
 </html>
